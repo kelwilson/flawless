6e6eab074cb767ed96e097331252608d35b42086
Kelwilson <kelwilson_achienu@icloud.com>
2025-04-18 15:46:35 -0700

    Add inverted borders to the main section

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./ui/nav-bar";
+import Navbar from "./ui/Nav-bar";
 import FlawlessLogo from "./ui/flawless-logo";
 import { lusitana } from "./ui/fonts";
 
@@ -39,10 +39,10 @@ export default function RootLayout({ children }) {
 
         <div className="flex items-center bg-slate-200 h-[150px] z-0 ">
           <div className="flex w-[45%] h-[100px]  relative">
-          <div className="h-[100px] w-[80%] bg-blue-400 mt-[2rem] z-10 flex justify-center items-center ml-10 rounded-t-3xl "><FlawlessLogo className={'m-4'}/></div>
-           <div className="h-[100px] w-[20%] bg-blue-400 skew-x-12 absolute right-4 top-8 rounded-tr-3xl"></div> 
+          <div className="h-[100px] w-[80%] bg-blue-400 mt-[2rem] z-10 flex justify-center items-center ml-10 rounded-t-5xl "><FlawlessLogo className={'m-4'}/></div>
+           {/* <div className="h-[100px] w-[20%] bg-blue-400 skew-x-12 absolute right-4 top-8 rounded-tr-3xl"></div>  */}
            </div>
-              <div className="flex items-end w-[55%] h-[100%] justify-evenly p-5 rounded-l-full border-4 border-indigo-600">
+              <div className="flex items-end w-[55%] h-[100%] justify-evenly p-5 rounded-l-full border-4 ">
   
               <header className= {`${lusitana.className} text-2xl flex justify-end items-center p-4 gap-4 h-16 `}>
                 <SignedOut>
