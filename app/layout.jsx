8d7ac2699fe916a973faed897343ff61484b0910
Kelwilson <kelwilson_achienu@icloud.com>
2025-04-28 21:16:01 -0700

    Add Navbar component to root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./ui/Nav-bar";
-import FlawlessLogo from "./ui/flawless-logo";
-import { lusitana } from "./ui/fonts";
+import NavBar from "./ui/Nav-bar";
+// import FlawlessLogo from "./ui/flawless-logo";
+// import { lusitana } from "./ui/fonts";
 
 import {
   ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
 } from '@clerk/nextjs'
 
 
@@ -36,26 +31,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
-        <div className="flex items-center bg-slate-200 h-[150px] z-0 ">
-          <div className="flex w-[45%] h-[100px]  relative">
-          <div className="h-[100px] w-[80%] bg-blue-400 mt-[2rem] z-10 flex justify-center items-center ml-10 rounded-t-5xl "><FlawlessLogo className={'m-4'}/></div>
-           {/* <div className="h-[100px] w-[20%] bg-blue-400 skew-x-12 absolute right-4 top-8 rounded-tr-3xl"></div>  */}
-           </div>
-              <div className="flex items-end w-[55%] h-[100%] justify-evenly p-5 rounded-l-full border-4 ">
-  
-              <header className= {`${lusitana.className} text-2xl flex justify-end items-center p-4 gap-4 h-16 `}>
-                <SignedOut>
-                  <SignInButton />
-                  <SignUpButton />
-                </SignedOut>
-                <SignedIn>
-                  <UserButton />
-                </SignedIn>
-              </header>
-        <Navbar/>
-        </div>
-        </div>
+        <NavBar/>
         {children}
       </body>
     </html>
